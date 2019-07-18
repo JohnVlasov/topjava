fd077ef4098a0742ad97e631024096d884094da3
VlaE <vlae@mail.ru>
2019-07-18 13:34:41 +0300

    HW06 1.1,1.2

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -7,4 +7,12 @@ import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcMealServiceTest extends AbstractMealServiceTest {
+/*  @Override
+    @Ignore
+    @Test
+    public void testValidation() throws Exception {
+        //Assume.assumeFalse(true);
+        super.testValidation();
+    }
+*/
 }
