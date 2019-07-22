2ff7e9d58468b9a95af309d52f7e653c584a0d7f
VlaE <vlae@mail.ru>
2019-07-22 21:45:52 +0300

    HW06

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.hibernate.hql.internal.ast.tree.Statement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -8,12 +9,19 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.List;
+import java.sql.ResultSet;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.lang.System.*;
 
 @Repository
+@Transactional(readOnly = true)
 public class JdbcUserRepository implements UserRepository {
 
     private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
@@ -35,6 +43,7 @@ public class JdbcUserRepository implements UserRepository {
     }
 
     @Override
+    @Transactional
     public User save(User user) {
         BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(user);
 
@@ -45,30 +54,73 @@ public class JdbcUserRepository implements UserRepository {
                 "UPDATE users SET name=:name, email=:email, password=:password, " +
                         "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id", parameterSource) == 0) {
             return null;
+        } else {
+            namedParameterJdbcTemplate.update("DELETE FROM user_roles WHERE user_id=:id", parameterSource);
+        }
+        Set<Role> set = (Set) parameterSource.getValue("roles");
+        int id = (Integer) parameterSource.getValue("id");
+        for (Role r : set) {
+            String sql = "INSERT INTO user_roles (user_id, role) values('" + id + "', '" + r.toString() + "')";
+            jdbcTemplate.update(sql);
         }
         return user;
     }
 
     @Override
+    @Transactional
     public boolean delete(int id) {
         return jdbcTemplate.update("DELETE FROM users WHERE id=?", id) != 0;
     }
 
     @Override
     public User get(int id) {
-        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
-        return DataAccessUtils.singleResult(users);
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("SELECT role FROM user_roles WHERE user_id=" + id);
+        Set<Role> set = list.stream().map(m -> Role.valueOf(m.get("role").toString())).collect(Collectors.toSet());
+
+        List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE users.id=?", ROW_MAPPER, id);
+        User user = DataAccessUtils.singleResult(users);
+        if (user != null) user.setRoles(set);
+        return user;
     }
 
     @Override
     public User getByEmail(String email) {
-//        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
         List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        return DataAccessUtils.singleResult(users);
+        User user = DataAccessUtils.singleResult(users);
+        if (user != null) {
+            List<Map<String, Object>> list = jdbcTemplate.queryForList("SELECT role FROM user_roles WHERE user_id=" + user.getId());
+            Set<Role> set = list.stream().map(m -> Role.valueOf(m.get("role").toString())).collect(Collectors.toSet());
+            user.setRoles(set);
+        }
+        return user;
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("SELECT user_id, role FROM user_roles");
+
+        Map<Integer, HashSet<Role>> map = new HashMap<>();
+
+        HashSet<Role> set = new HashSet<>();
+
+        for (Map<String, Object> m : list) {
+            int userId = Integer.parseInt(m.get("user_id").toString());
+            Role userRole = Role.valueOf(m.get("role").toString());
+
+            if (map.get(userId) == null) {
+                set.add(userRole);
+                map.put(userId, set);
+
+            } else {
+                set = map.get(userId);
+                set.add(userRole);
+                map.put(userId, set);
+            }
+            set = new HashSet<>();
+        }
+        List<User> users = jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        users.forEach(user -> user.setRoles(map.get(user.getId())));
+        return users;
     }
 }
